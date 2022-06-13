fe433a1a3c8571fa8d7d36a22bba7b0f56019695
涛 <oncwnuJR4Rnqg7X2rpUUyT90PUCo@git.weixin.qq.com>
2022-06-13 17:41:09 +0800

    链接数据库完成

--- router/user.js
+++ router/user.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const Result = require('../models/Result')
+const {querySql} =require('../db')
 
 const router = express.Router()
 
 router.post('/login', function(req, res, next) {
   console.log(req.body)
   const {username,password} =req.body
+  querySql('select * from admin_user').then(res =>{
+    console.log(res)
+  }).catch(err =>{
+    console.log(err)
+  })
   if(username =='admin'&&password == '111111'){
     new Result({token:'aksjdakgdshagdhj'},'登录成功').success(res)
   }else{
