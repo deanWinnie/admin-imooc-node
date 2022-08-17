a0251a78092fb1a13e957c22ab0a543ab2125030
涛 <oncwnuJR4Rnqg7X2rpUUyT90PUCo@git.weixin.qq.com>
2022-08-17 17:59:05 +0800

    上传接口完成

--- utils/constant.js
+++ utils/constant.js
@@ -1,3 +1,6 @@
+const { env } =require('./env');
+const UPLOAD_PATH = env ==='dev'?'D:/upload/admin-upload/ebook':'/root/upload/admin-upload/ebook';
+
 module.exports = {
   CODE_ERROR: -1,
   CODE_SUCCESS: 0,
@@ -5,5 +8,6 @@ module.exports = {
   debug:true,
   PWD_SALT: 'admin_imooc_node',
   PRIVATE_KEY:'mywen_deanwinnie',
-  JWT_EXPIRED:60 * 60
+  JWT_EXPIRED:60 * 60,
+  UPLOAD_PATH
 }
